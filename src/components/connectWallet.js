57394b35b7f46037656a56a05762c387230136e3
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-02-27 15:57:59 +0900

    chore: minor style changes

--- src/components/connectWallet.js
+++ src/components/connectWallet.js
@@ -1,8 +1,9 @@
-export const connectWallet = async(setWalletAddress) => {
+export const connectWallet = async (setWalletAddress) => {
     const { solana } = window;
     if (solana) {
         const response = await solana.connect();
         console.log('Connected with Public Key:', response.publicKey.toString());
+        console.log(">>>", setWalletAddress)
         setWalletAddress(response.publicKey.toString());
     }
     return;
