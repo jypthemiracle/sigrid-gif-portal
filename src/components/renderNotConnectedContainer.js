f1984334fb702341086b1547ea5152a98cfe7d6a
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-02-27 16:23:32 +0900

    fix: connectWallet to have setWalletAddress hooks

--- src/components/renderNotConnectedContainer.js
+++ src/components/renderNotConnectedContainer.js
@@ -1,7 +1,7 @@
 import { connectWallet } from "./connectWallet";
 
-export const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button" onClick={connectWallet}>
+export const renderNotConnectedContainer = (setWalletAddress) => (
+    <button className="cta-button connect-wallet-button" onClick={() => connectWallet(setWalletAddress)}>
         Connect To Wallet
     </button>
 )
