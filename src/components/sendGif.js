df71738ef384771d9a266d7e521f222dbd3f6e47
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-02-27 15:57:27 +0900

    feat: sendGif to communicate with rpc programs

--- src/components/sendGif.js
+++ src/components/sendGif.js
@@ -1,9 +1,37 @@
-export const sendGif = async (inputValue, gifList, setGifList, setInputValue) => {
-  if (inputValue.length > 0) {
-    console.log("Gif link:", inputValue);
-    setGifList([...gifList, inputValue]);
-    setInputValue('');
-  } else {
-    console.log("Empty input. Try again.");
+import idl from "../idl.json";
+import { getProvider } from "./getProvider";
+import { Program, Provider, web3 } from "@project-serum/anchor";
+import { SystemProgram } from "@solana/web3.js";
+import { getGifList } from "./getGifList";
+
+export const sendGif = async (
+  inputValue,
+  gifList,
+  setGifList,
+  setInputValue,
+  programID,
+  baseAccount
+) => {
+  if (inputValue.length === 0) {
+    console.log("No gif link given!");
+    return;
+  }
+  setInputValue("");
+  console.log("Gif link:", inputValue);
+  try {
+    const provider = getProvider();
+    const program = new Program(idl, programID, provider);
+
+    await program.rpc.addGif(inputValue, {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    });
+    console.log("GIF successfully sent to program", inputValue);
+
+    await getGifList();
+  } catch (error) {
+    console.log("Error sending GIF:", error);
   }
 };
