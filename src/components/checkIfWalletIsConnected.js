7bc8bfb0df72d5e59d5fea988f2e172a94cae3a5
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-02-27 15:56:41 +0900

    chore: minor change - reduce the amounts of SIGRID_GIFS - console.log from alert

--- src/components/checkIfWalletIsConnected.js
+++ src/components/checkIfWalletIsConnected.js
@@ -2,7 +2,7 @@ export const checkIfWalletIsConnected = async () => {
   try {
     const { solana } = window;
     if (solana && solana.isPhantom) {
-      alert("Phantom Wallet is found!");
+      console.log("Phantom Wallet is found!");
       
       const response = await solana.connect({ onlyIfTrusted: true });
       console.log("Connected with Public Key: ", response.publicKey.toString());
@@ -13,7 +13,7 @@ export const checkIfWalletIsConnected = async () => {
       "Something gone wrong: It seems that Phantom Wallet 👻 is not installed yet."
     );
   } catch (error) {
-    console.log("something gone wrong. Error: ", error);
+    alert("Hey, please unlock your Phantom Wallet to connect!");
     console.log(error);
   }
 };
