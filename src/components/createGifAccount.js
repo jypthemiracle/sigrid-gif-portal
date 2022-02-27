a2f16695a02e7907c86c1c01fca01fd15e9027a5
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-02-27 16:56:32 +0900

    fix: baseAccount not be renewed

--- src/components/createGifAccount.js
+++ src/components/createGifAccount.js
@@ -8,19 +8,20 @@ export const createGifAccount = async (programID, baseAccount, setGifList) => {
   try {
     const provider = getProvider();
     const program = new Program(idl, programID, provider);
-    console.log("baseAccount >>>", baseAccount);
-    await program.rpc.startStuffOff({
-      accounts: {
-        baseAccount: baseAccount.publicKey,
-        user: provider.wallet.publicKey,
-        systemProgram: SystemProgram.programId,
-      },
-      signers: [baseAccount],
-    });
-    console.log(
-      "Created a new BaseAccount w/ address: ",
-      baseAccount.publicKey.toString()
-    );
+    if (baseAccount === null) {
+      await program.rpc.startStuffOff({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount],
+      });
+      console.log(
+        "Created a new BaseAccount w/ address: ",
+        baseAccount.publicKey.toString()
+      );
+    }
     await getGifList(programID, baseAccount, setGifList);
   } catch (error) {
     console.log("Error creating BaseAccount account: ", error);
